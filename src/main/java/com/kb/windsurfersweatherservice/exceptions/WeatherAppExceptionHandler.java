81e125716071298c034fb4f75946c688412c6126
kbledzki <krzysztofbledzki@gmail.com>
2023-12-03 23:03:37 +0100

    KB_T03_010-upgrade handler

--- src/main/java/com/kb/windsurfersweatherservice/exceptions/WeatherAppExceptionHandler.java
+++ src/main/java/com/kb/windsurfersweatherservice/exceptions/WeatherAppExceptionHandler.java
@@ -4,12 +4,17 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class WeatherAppExceptionHandler {
 
     @ExceptionHandler(WeatherAppException.class)
-    public ResponseEntity<ErrorInfo> handleException(WeatherAppException e) {
+    public ResponseEntity<Object> handleException(WeatherAppException e, WebRequest request) {
         HttpStatus httpStatus = HttpStatus.INTERNAL_SERVER_ERROR;
         if (WeatherError.WRONG_DATE_FORMAT.equals(e.getWeatherError())
                 || WeatherError.BAD_WEATHER_CONDITIONS_FOR_ALL_CITIES.equals(e.getWeatherError())
@@ -17,6 +22,11 @@ public class WeatherAppExceptionHandler {
                 || WeatherError.TOO_DISTANT_DATE.equals(e.getWeatherError())) {
             httpStatus = HttpStatus.BAD_REQUEST;
         }
-        return ResponseEntity.status(httpStatus).body(new ErrorInfo(e.getWeatherError().getMessage()));
+        Map<String, Object> map = new HashMap<>();
+        map.put("timestamp", LocalDateTime.now());
+        map.put("status", httpStatus.value());
+        map.put("message", e.getWeatherError().getMessage());
+        map.put("path", request.getDescription(false));
+        return new ResponseEntity<>(map, httpStatus);
     }
 }
